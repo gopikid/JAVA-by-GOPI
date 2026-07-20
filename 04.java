947fafda7d7200dc32c92a1f98a7ff732eb4387b
GOPINATH V <23ec033@excelcolleges.com>
2026-07-20 15:20:26 +0530

    Create 04.java

---
+++ 04.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+    int[]marks={111,222,333,444,555};
+    String[]subject={"Bharath","Sakthivel","Rutresh","Jaga","Gopi"};
+    for(int i=0;i<marks.length;i++)
+    {
+         System.out.println(subject[i]+"="+marks[i]);
+        }
+  }
+}
