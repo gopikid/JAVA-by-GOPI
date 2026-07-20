261f80fd9448bea46b6cf0d6fa24e291a639bdd2
GOPINATH V <23ec033@excelcolleges.com>
2026-07-20 15:17:48 +0530

    Add Main class to display subject marks

---
+++ 03.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+    int[]marks={92,94,96,98,100};
+    String[]subject={"Tamil","English","Maths","Science","Social"};
+    for(int i=0;i<marks.length;i++)
+    {
+         System.out.println(subject[i]+"="+marks[i]);
+        }
+  }
+}
